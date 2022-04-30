f9e7c7dc53f368cb0d3db17230facc0ca6e01be0
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2022-04-30 16:10:34 +0600

    carousel added

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../images/banner1.png';
+import banner2 from '../../images/banner2.png';
+import banner3 from '../../images/banner3.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
         <div>
-            This is banner
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
 
-export default Banner;
+export default Banner; 
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            hello from headr
+            <Navbar bg="primary" collapseOnSelect expand="lg" sticky='top' variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Electro<span className='pink'>Mart</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto"> 
+                            <Nav.Link as={Link} to="blog">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
